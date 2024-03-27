7828397e75efadb78e55b7d0d004804758f651b2
sanju khetavath <rathodsanju399@gmail.com>
2024-03-27 12:43:31 +1100

    updated main file

--- infra/main.tf
+++ infra/main.tf
@@ -47,14 +47,6 @@ resource "azurerm_linux_web_app" "app" {
     }
   }
 }
-provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
-    }
-  }
-}
 
 data "azurerm_client_config" "current" {}
 
--- infra/providers.tf
+++ infra/providers.tf
@@ -20,3 +20,11 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
 }
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
