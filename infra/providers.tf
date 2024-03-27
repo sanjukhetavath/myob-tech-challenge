84bbf8a2eaafb2920b81c1deb5cb7cbb63b4f050
sanju khetavath <rathodsanju399@gmail.com>
2024-03-27 12:50:20 +1100

    updated main file

--- infra/providers.tf
+++ infra/providers.tf
@@ -19,9 +19,4 @@ provider "azurerm" {
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
-}
-    key_vault {
-      purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
-    }
-  }
+}   
